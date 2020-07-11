b8b852dfb062201357bd1277cedbfd6c3f6d84ed
Victor Torres <torres.victor136@gmail.com>
2020-07-11 00:07:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,24 @@ def WIN_COMBINATIONS = [
   [0, 4, 8],
   [6, 4, 2]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combination|}
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+  
